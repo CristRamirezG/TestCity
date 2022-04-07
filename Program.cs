0f6cc4024b5216cb1f034d00f915f36be630412f
Cristobal Andrés Ramirez González <72883591+CristRamirezG@users.noreply.github.com>
2022-04-07 16:02:00 -0400

    test1
    ....

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net;
+using Newtonsoft.Json;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            String url = "http://localhost:8080/Rut";
+            Console.WriteLine(url);
+
+            WebClient client = new WebClient();
+            string Json = client.DownloadString(url);  
+
+            dynamic m = JsonConvert.DeserializeObject(Json);
+            Console.WriteLine(Json);
+        }
+    }
+}
